4e530d1c0ef2c8fee6130a221d1d3b4047b30f96
sergey-png <svsobolev@miem.hse.ru>
2022-04-15 18:03:11 +0300

    edited .gitignore

--- main.py
+++ main.py
@@ -2,37 +2,17 @@ import sys
 
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from base import Ui_MainWindow
 
-from PyQt5.QtWidgets import QApplication, QLabel, QScrollArea, QVBoxLayout, QWidget
 
-
-def main():
-    app = QApplication([])
-    layout = QVBoxLayout()
-    scroll = QScrollArea()
-    info = QSerialPortInfo.availablePorts()
-    for i in info:
-        s = "Name: " + i.portName() + "\n"
-        s += "Location: " + i.systemLocation() + "\n"
-        s += "Description: " + i.description() + "\n"
-        s += "Manufacturer: " + i.manufacturer() + "\n"
-        s += "Serial Number: " + i.serialNumber() + "\n"
-        s += "System Location: " + i.systemLocation() + "\n"
-        s += "Vendor Identifier: " + str(i.vendorIdentifier()) + "\n"
-        s += "Product Identifier: " + str(i.productIdentifier()) + "\n"
-        s += "Busy: " + str(i.isBusy()) + "\n"
-        s += "------------------------------\n"
-        layout.addWidget(QLabel(s))
-    if not info:
-        layout.addWidget(QLabel("No serial ports found"))
-    workPage = QWidget()
-    workPage.setLayout(layout)
-    scroll.setWindowTitle("Serial Port Info")
-    scroll.setWidget(workPage)
-    scroll.show()
-    return app.exec_()
+class MyWin(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWin, self).__init__(parent)
+        self.setupUi(self)
 
 
 if __name__ == "__main__":
-    main()
-    sys.exit(0)
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    myapp.show()
+    sys.exit(app.exec_())
